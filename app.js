465459b38a3078f7b79c9fe90c103ba00a32225c
Oliver Smith <77962172+OliverPSmith@users.noreply.github.com>
2022-03-14 19:48:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
-//const homePage = document.querySelector('.home-page');
 const nameHero = document.querySelector('.name-hero');
 const main = document.querySelector('.main');
 const body = document.querySelector('.body');
@@ -30,53 +29,6 @@ hamburger.addEventListener('click', () => {
 }); 
 
 
-/*
-hamburger.addEventListener('click', () => {
-    if (hamburger.classList.contains('active')) {
-        hamburger.classList.remove('active');
-        nav.classList.remove('active');
-        homePage.classList.remove('active');
-        console.log('hamburger remove menu');
-    }   else {
-        hamburger.classList.add('active');
-        nav.classList.add('active');
-        homePage.classList.add('active');
-        console.log('hamburger add menu');
-    }
-
-});     */
-
-/*
-html.addEventListener('click', (e) => {
-    if (e.target !== hamburger && hamburger.classList.contains('active')) {
-        hamburger.classList.remove('active');
-        nav.classList.remove('active');
-        homePage.classList.remove('active');
-        console.log('window close menu');
-    }   else {
-        console.log('window click - do nothing');
-    }
-});     */
-
-/*
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    nav.classList.toggle('active');
-    homePage.classList.toggle('active');
-    console.log('menu selected');
-
-    html.addEventListener('click', (e) => {
-        if (e.target !== hamburger && hamburger.classList.contains('active')) {
-            hamburger.classList.toggle('active');
-            nav.classList.toggle('active');
-            homePage.classList.toggle('active');
-            console.log('window close');
-        }
-    })
-
-});     */
-
-
 /******** hidden nav bar on scroll */
 
 let newScrollPosition = 0;
@@ -87,29 +39,14 @@ const header = document.getElementById('header-menu');
 window.addEventListener('scroll', e => {
     lastScrollPosition = window.scrollY;
 
-    if (newScrollPosition < lastScrollPosition && lastScrollPosition > 50) {
-        nameHero.classList.remove('slide-down');
-        nameHero.classList.add('slide-up');
-        nameHero.style.transition = "all .5s";  
-
+    if (newScrollPosition < lastScrollPosition && lastScrollPosition > 20) {
         hamburger.classList.remove('slide-down');
         hamburger.classList.add('slide-up');
         hamburger.style.transition = "all .5s";  
-      /*  header.classList.remove('slide-down');
-        header.classList.add('slide-up');
-        header.style.transition = "all .5s";    */
     }   else if (newScrollPosition > lastScrollPosition) {
-        nameHero.classList.remove('slide-up');
-        nameHero.classList.add('slide-down');
-        nameHero.style.transition = "all .5s";  
-
         hamburger.classList.remove('slide-up');
         hamburger.classList.add('slide-down');
         hamburger.style.transition = "all .5s";  
-      /*  header.classList.remove('slide-up');
-        header.classList.add('slide-down');
-        header.style.transition = "all .5s";    */
     }
-
     newScrollPosition = lastScrollPosition;
 });
